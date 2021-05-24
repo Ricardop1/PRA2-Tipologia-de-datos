992435713ba94f3cdb5ba5b1efb765585f0a5874
Ricardo Santos <r_patricio@live.com.pt>
2021-05-24 17:53:25 +0200

    Arreglados nombres y Estructura

--- Codigo_Pra2.R
+++ Codigo_Pra2.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(caret)
 library(nortest)
+library(ranger)
+library(rpart.plot)
 
 # 2. Integraci?n y selecci?n de los datos de inter?s a analizar
 
@@ -22,26 +24,31 @@ titanic$Pclass <- as.factor(titanic$Pclass)
 titanic$Sex <- as.factor(titanic$Sex)
 titanic$Embarked <- as.factor(titanic$Embarked)
 
+levels(titanic$Survived)
+levels(titanic$Pclass)
+levels(titanic$Sex)
+levels(titanic$Embarked)
+
 
 # 3. Limpieza de los datos
 
 ## ¿0's o elementos vacíos?
 
 ###  0's
-
 where.ceros <- function(x){
   which(x[!is.na(x)] == 0)
   }
 
 titanic.ceros <- lapply(titanic[,c("Fare", "Age")], where.ceros)
+titanic.ceros
 
 ### NA
-
 where.na <- function(x){
   which(is.na(x) == TRUE)
 }
 
 titanic.na <- lapply(titanic[, c("Age", "SibSp", "Parch", "Fare")], where.na)
+titanic.na
 
 
 ## Gestión de los casos.
@@ -71,7 +78,7 @@ predicted_age <- train(
   importance = 'impurity'
 )
 
-titanic$Age[age.na] <- predict(predicted_age, titanic[titanic$Age[age.na], ])
+titanic$Age[age.na] <- predict(predicted_age, titanic[age.na,])
 
 summary(titanic$Age[age.na])
 summary(titanic$Age[-age.na])
@@ -81,49 +88,43 @@ summary(titanic$Age[-age.na])
 embarked.na <- which(titanic$Embarked == "")
 titanic$Embarked[embarked.na] <- NA
 
-titanic[embarked.na, c(2, 7)]
+titanic[embarked.na, c("Pclass", "Fare")]
 
+#filtramos por clase y puerta de embarque
+titanic.C <- titanic[titanic$Embarked =="C" & titanic$Pclass =="1",]
+titanic.Q <- titanic[titanic$Embarked =="Q" & titanic$Pclass =="1",]
+titanic.S <- titanic[titanic$Embarked =="S" & titanic$Pclass =="1",]
 
-titanic %>%
-  group_by(Embarked, Pclass) %>%
-  filter(Pclass == "1") %>%
-  summarise(mfare = median(Fare),
-            n = n())
-
-titanic$Embarked[embarked.na] <- "C"
-levels(titanic$Embarked) <- factor(titanic$Embarked)
+median(titanic.C$Fare, na.rm = TRUE)
+median(titanic.Q$Fare, na.rm = TRUE)
+median(titanic.S$Fare, na.rm = TRUE)
 
 ## Identificación y tratamiento de valores extremos
 
-detect.outliers <- function(x){
-  iqr <- quantile(x)
-  lower.iqr <- iqr[2]
-  upper.iqr <- iqr[4]
-  x.iqr <- upper.iqr - lower.iqr
-  
-  upper.threshold <- (x.iqr*1.5) + upper.iqr
-  lower.threshold <- lower.iqr - (x.iqr*1.5)
-  
-  values <- x < lower.threshold | x > upper.threshold
-  return(values)
-}
+out.Age <- boxplot(titanic[,"Age"])$out
+out.SibSp <- boxplot(titanic[,"SibSp"])$out
+out.Parch <- boxplot(titanic[,"Parch"])$out
+out.Fare <- boxplot(titanic[,"Fare"])$out 
 
 
-outliers <- lapply(titanic[, c("Age", "SibSp", "Parch", "Fare")], detect.outliers)
-lapply(titanic[, c("Age", "SibSp", "Parch", "Fare")], boxplot)
+length(out.Age)
+length(out.SibSp)
+length(out.Parch)
+length(out.Fare)
 
-which(titanic$Fare > 500)
-titanic_raw[c(259, 680, 738), ] # Hemos hecho una pequeña vuelta a este dataset tan sólo para recuperar el nombre de Cardeza
+billetes.caros <- which(titanic$Fare > 500)
+titanic_raw$Name[billetes.caros] # Hemos hecho una pequeña vuelta a este dataset tan sólo para recuperar el nombre de Cardeza
 
 
 # 4. Análisis de los datos
 ## 4.1. Selección de los grupos de datos que se quieren analizar/comparar
 titanic.primeraClase <- titanic[titanic$Pclass == 1,]
-titanic.segundaClase <- titanic[titanic$Pclass == 3,]
+titanic.segundaClase <- titanic[titanic$Pclass == 2,]
 titanic.terceraClase <- titanic[titanic$Pclass == 3,]
 
 titanic.mujeres <- titanic[titanic$Sex == "female",]
 titanic.hombres <- titanic[titanic$Sex == "male",]
+
 
 titanic.supervivientes <- titanic[titanic$Survived == 1,]
 titanic.fallecidos <- titanic[titanic$Survived == 0,]
@@ -131,25 +132,97 @@ titanic.fallecidos <- titanic[titanic$Survived == 0,]
 
 ## 4.2. Comprobación de la normalidad y la homogeneidad de la varianza
 
+#normalidad
+lapply(titanic[,c("Age", "SibSp", "Parch", "Fare")], shapiro.test)
 
-lapply(titanic[,c("Age", "SibSp", "Parch", "Fare")], ad.test)
+#homohgeneidad de varianzas
 
+fligner.test(Age ~ Survived, data = titanic)
 
-
-
-
-
-
-
-
+boxplot(titanic.supervivientes[,"Age"])
+boxplot(titanic.fallecidos[,"Age"])
 
 
+## Aplicación de pruebas estadísticas para comparar los grupos de datos
+
+### Estudio de variables significativas
+
+modelo.Surv <- glm(Survived ~ Pclass + Sex + SibSp + Parch + Fare + Age +  Embarked, data = titanic, family = "binomial"(link=logit))
+summary(modelo.Surv)
+
+#Vamos a comprobar si efectivamente a mayor número de hermanos menor era el rango de edad.
+
+plot(titanic$SibSp, titanic$Age,
+     xlab="Número de hermanos",
+     ylab="Edad")
+
+#Comprobamos predicciones
+
+test <- data.frame( Pclass= c("1","1","1","1"),
+                    Sex = c("female","male","female","male"),
+                    SibSp = c(3,3,3,3),
+                    Parch = c(1,1,1,1),
+                    Fare = c(200,200,200,200),
+                    Age = c(15,15,50,50),
+                    Embarked = c("C","C","C","C")
+                    )
+
+predict(modelo.Surv, test, type = "response")
+
+
+### Estudio de la relación de Survived con Age, Sex y Pclass
+
+tabla.SurvPclass <- table(titanic$Survived, titanic$Pclass)
+tabla.SurvSex <- table(titanic$Survived, titanic$Sex)
+
+chisq.test(tabla.SurvPclass)
+chisq.test(tabla.SurvSex)
+chisq.test(x =titanic$Survived, y = titanic$Age )
+
+### ¿La proporción de pasajeros fallecidos de Clase 3 es realmente mayor que la proporción de pasajeros fallecidos de clase 1?
+
+(numPrimera <- length(titanic.primeraClase$Survived))
+(numTercera <- length(titanic.terceraClase$Survived))
+
+(p1 <- sum(titanic.terceraClase$Survived == 0) / numTercera)
+(p2 <- sum(titanic.primeraClase$Survived == 0) / numPrimera)
 
 
 
+#Comprobación
+success <- c(p1*numTercera,p2*numPrimera)
+n <- c(numTercera,numPrimera)
+prop.test( success, n, alternative="greater", correct=FALSE)
+
+#representacion de resultados y conclusiones
+
+plot(titanic$Parch, titanic$Age,
+     xlab="Miembros en la familia",
+     ylab="Edad")
+
+interaction.plot(titanic$Survived,titanic$Sex,titanic$Age,
+                 xlab = "Survived", ylab = "Edad")     
+
+supervivencia <- rpart(Survived~.,data=titanic,method="class",
+                        minsplit=1,cp=.02,parms = list(split ="gini"))
+
+rpart.plot(supervivencia,type = 3,extra = 100 ,clip.right.lab = FALSE)                 
+
+# Exportación del dataset
 
 
 
+str(titanic)
 
+titanic_raw$Survived <- titanic$Survived
+titanic_raw$Pclass   <- titanic$Pclass  
+titanic_raw$Sex      <- titanic$Sex     
+titanic_raw$Age      <- titanic$Age     
+titanic_raw$SibSp    <- titanic$SibSp   
+titanic_raw$Parch    <- titanic$Parch   
+titanic_raw$Fare     <- titanic$Fare    
+titanic_raw$Embarked <- titanic$Embarked
 
+write.csv(titanic_raw,"./csv/Titanic_clean.csv",
+          row.names=FALSE, fileEncoding = 'UTF-8')
 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PRA2-Tipologia-de-datos
-Repositorio para la realización de la practica 2 de la asignatura Tipologia y Ciclo de Vida de los Datos del master en Data Science de la UOC
+Repositorio para la realización de la practica 2 de la asignatura Tipologia y Ciclo de Vida de los Datos del master en Data Science de la UOC elaborada por **Fernando Muñoz Martín** y **Ricardo Santos Patrício**.
+
+## Descripción Archivos
+
+La estructura del repositorio consiste en:
+
+    - **pra2_Analisis.pdf**: documento pdf que contiene el código realizado y las respuestas a las cuestiones propuestas;
+    - **pra2_Analisis.Rmd**: archivo Rmd creado para la estructuración del archivo PDF juntamente con el código;
+    - **Codigo_Pra2.R**: archivo R que presenta solamente el código elaborado;
+    - **csv**: carpeta con el archivo csv inicial y el archivo csv resultante;
+        - **train.csv**: archivo csv con los datos iniciales del dataset de Titanic;
+        -**Titanic_clean.csv**: archivo csv con los datos resultantes del analisis realizado
+    
